84b39628472410623ba9aaf31b3a53d4e760eb24
navo <navodevs@proton.me>
2024-11-24 13:11:55 +0200

    Initial commit

---
+++ frontend/src/pages/Shop.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+function Shop() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-500">
+      <p className="text-3xl text-red-500">SIGMA</p>
+    </div>
+  );
+}
+
+export default Shop;
